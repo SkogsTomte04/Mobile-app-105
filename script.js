23215a5ef016af71f87a3473ae2b8f180ea8d817
William Möller <williammoller321@gmail.com>
2024-04-07 02:35:10 +1300

    prototype feed

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function $(x){
+    return document.getElementById(x);
+}
+
 function getJSONData(){
     fetch('./Assets/data.json')
         .then((respond) => {
@@ -5,10 +9,38 @@ function getJSONData(){
                 throw new Error('HTTP error! Status: ${respond.status}');
             }
             return respond.json();
-        })
-        .then((data) => console.log(data))
-        .catch((error) => console.error("Unable to fetch data", error));
-}
+        }).then(function (obj) {
+            let list = $("dataFeed");
 
-const jsonData = JSON.parse(getJSONData());
-document.getElementById("username").innerHTML = jsonData.employees[1].firstName;
+
+            for(i = 0; i < obj.post.length; ++i){
+                let div = document.createElement('div');
+                div.classList.add("container");
+
+                let li = document.createElement('li');
+                li.classList.add("username")
+                li.innerText = obj.post[i].username;
+                div.appendChild(li)
+
+                let imgcontainer = document.createElement('div');
+                imgcontainer.classList.add("content")
+                let img = document.createElement('img');
+                img.src=obj.post[i].content;
+                imgcontainer.appendChild(img);
+                div.appendChild(imgcontainer)
+
+                let footer = document.createElement('ul');
+                footer.classList.add("footer");
+                let desc = document.createElement('li');
+                desc.innerText = obj.post[i].description;
+                footer.appendChild(desc);
+                div.appendChild(footer);
+
+                list.appendChild(div);
+            };
+            
+        }).catch(function (error) { 
+            console.error("Unable to fetch data", error);
+        })
+}
+getJSONData();
