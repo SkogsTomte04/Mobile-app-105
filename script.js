79cab0f16412389a0f7ca7d62c6de67c6173c469
William Möller <williammoller321@gmail.com>
2024-04-07 15:07:06 +1200

    feedpage styling update

--- script.js
+++ script.js
@@ -17,20 +17,40 @@ function getJSONData(){
                 let div = document.createElement('div');
                 div.classList.add("container");
 
+                let header = document.createElement('div');
+                header.classList.add("header");
+                let profile = document.createElement('img');
+                profile.src=obj.post[i].profilepic;
+                header.appendChild(profile);
+                let ul = document.createElement('ul');
+                ul.classList.add("userdata")
+
                 let li = document.createElement('li');
-                li.classList.add("username")
+                li.classList.add("username");
                 li.innerText = obj.post[i].username;
-                div.appendChild(li)
+                let loc = document.createElement('li');
+                loc.classList.add("location");
+                loc.innerText = obj.post[i].location;
+                ul.appendChild(li);
+                ul.appendChild(loc);
+                header.appendChild(ul)
+                div.appendChild(header)
 
                 let imgcontainer = document.createElement('div');
                 imgcontainer.classList.add("content")
-                let img = document.createElement('img');
-                img.src=obj.post[i].content;
-                imgcontainer.appendChild(img);
+                let content = document.createElement('img');
+                content.src=obj.post[i].content;
+                imgcontainer.appendChild(content);
                 div.appendChild(imgcontainer)
+
+
 
                 let footer = document.createElement('ul');
                 footer.classList.add("footer");
+                let title = document.createElement('li');
+                title.classList.add("title");
+                title.innerText = obj.post[i].title
+                footer.appendChild(title);
                 let desc = document.createElement('li');
                 desc.innerText = obj.post[i].description;
                 footer.appendChild(desc);
